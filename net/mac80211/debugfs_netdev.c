b8de6c92c10982adaf5de8f6366bc9ef35eb342e
Ashok Nagarajan <ashok@cozybit.com>
2012-05-07 21:00:32 -0700

    mac80211: Add debugfs entry for mesh ht_opmode
    Signed-off-by: Ashok Nagarajan <ashok@cozybit.com>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- net/mac80211/debugfs_netdev.c
+++ net/mac80211/debugfs_netdev.c
@@ -507,6 +507,7 @@ IEEE80211_IF_FILE(dot11MeshHWMPRannInterval,
 		  u.mesh.mshcfg.dot11MeshHWMPRannInterval, DEC);
 IEEE80211_IF_FILE(dot11MeshForwarding, u.mesh.mshcfg.dot11MeshForwarding, DEC);
 IEEE80211_IF_FILE(rssi_threshold, u.mesh.mshcfg.rssi_threshold, DEC);
+IEEE80211_IF_FILE(ht_opmode, u.mesh.mshcfg.ht_opmode, DEC);
 IEEE80211_IF_FILE(dot11MeshHWMPactivePathToRootTimeout,
 		  u.mesh.mshcfg.dot11MeshHWMPactivePathToRootTimeout, DEC);
 IEEE80211_IF_FILE(dot11MeshHWMProotInterval,
@@ -610,6 +611,7 @@ static void add_mesh_config(struct ieee80211_sub_if_data *sdata)
 	MESHPARAMS_ADD(dot11MeshHWMPRannInterval);
 	MESHPARAMS_ADD(dot11MeshGateAnnouncementProtocol);
 	MESHPARAMS_ADD(rssi_threshold);
+	MESHPARAMS_ADD(ht_opmode);
 	MESHPARAMS_ADD(dot11MeshHWMPactivePathToRootTimeout);
 	MESHPARAMS_ADD(dot11MeshHWMProotInterval);
 #undef MESHPARAMS_ADD
