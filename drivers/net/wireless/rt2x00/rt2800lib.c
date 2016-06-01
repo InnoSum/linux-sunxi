05ca4f97eb55811bf60f7c13fa926f17cf500a56
Jeroen Roovers <jer@airfi.aero>
2016-06-01 17:55:08 +0200

    Revert "From 2557654df14b95141f1410127c33ed1661a8abbb Mon Sep 17 00:00:00 2001"
    This reverts commit d107fc239cb7e7d93327ba4e3f3a0d73cd861068.
    
    We already had this hard-wired for our purposes and this patch depends
    on new mac80211 functions we don't have.

--- drivers/net/wireless/rt2x00/rt2800lib.c
+++ drivers/net/wireless/rt2x00/rt2800lib.c
@@ -7494,10 +7494,6 @@ static int rt2800_probe_hw_mode(struct rt2x00_dev *rt2x00dev)
 		rt2x00dev->hw->flags |=
 			IEEE80211_HW_HOST_BROADCAST_PS_BUFFERING;
 
-	/* Set MFP if HW crypto is disabled. */
-	if (rt2800_hwcrypt_disabled(rt2x00dev))
-		ieee80211_hw_set(rt2x00dev->hw, MFP_CAPABLE);
-
 	SET_IEEE80211_DEV(rt2x00dev->hw, rt2x00dev->dev);
 	SET_IEEE80211_PERM_ADDR(rt2x00dev->hw,
 				rt2800_eeprom_addr(rt2x00dev,
