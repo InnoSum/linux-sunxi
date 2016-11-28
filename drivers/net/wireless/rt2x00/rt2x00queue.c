81640fbb5376dcc013a25c6faa25d0abaa81046b
Jeroen Roovers <jer@airfi.aero>
2016-11-28 17:00:52 +0100

    Revert rejected patch
    http://marc.info/?l=linux-wireless&m=147931166822663&w=2

--- drivers/net/wireless/rt2x00/rt2x00queue.c
+++ drivers/net/wireless/rt2x00/rt2x00queue.c
@@ -305,19 +305,14 @@ static void rt2x00queue_create_tx_descriptor_ht(struct rt2x00_dev *rt2x00dev,
 	struct ieee80211_tx_info *tx_info = IEEE80211_SKB_CB(skb);
 	struct ieee80211_tx_rate *txrate = &tx_info->control.rates[0];
 	struct ieee80211_hdr *hdr = (struct ieee80211_hdr *)skb->data;
-	u8 ba_size = 0;
+	struct rt2x00_sta *sta_priv = NULL;
 
 	if (sta) {
-		struct rt2x00_sta *sta_priv = sta_to_rt2x00_sta(sta);
+		txdesc->u.ht.mpdu_density =
+		    sta->ht_cap.ampdu_density;
 
-		txdesc->u.ht.mpdu_density = sta->ht_cap.ampdu_density;
+		sta_priv = sta_to_rt2x00_sta(sta);
 		txdesc->u.ht.wcid = sta_priv->wcid;
-
-		if (!(tx_info->flags & IEEE80211_TX_CTL_RATE_CTRL_PROBE)) {
-			ba_size = IEEE80211_MIN_AMPDU_BUF;
-			ba_size <<= sta->ht_cap.ampdu_factor;
-			ba_size = min_t(int, 63, ba_size - 1);
-		}
 	}
 
 	/*
@@ -353,7 +348,7 @@ static void rt2x00queue_create_tx_descriptor_ht(struct rt2x00_dev *rt2x00dev,
 		return;
 	}
 
-	txdesc->u.ht.ba_size = ba_size;
+	txdesc->u.ht.ba_size = 7;	/* FIXME: What value is needed? */
 
 	/*
 	 * Only one STBC stream is supported for now.
