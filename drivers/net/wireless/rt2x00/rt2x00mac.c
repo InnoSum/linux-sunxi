2bc4a4363dfc8e751b7622f3265c310f69f7b690
Jeroen Roovers <jer@airfi.aero>
2017-01-18 11:50:29 +0100

    Apply backported 6 January 2017 patches

--- drivers/net/wireless/rt2x00/rt2x00mac.c
+++ drivers/net/wireless/rt2x00/rt2x00mac.c
@@ -747,7 +747,8 @@ void rt2x00mac_flush(struct ieee80211_hw *hw, bool drop)
 		return;
 
 	tx_queue_for_each(rt2x00dev, queue)
-		rt2x00queue_flush_queue(queue, drop);
+		if (!rt2x00queue_empty(queue))
+			rt2x00queue_flush_queue(queue, drop);
 }
 EXPORT_SYMBOL_GPL(rt2x00mac_flush);
 
