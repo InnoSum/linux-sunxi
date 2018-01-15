baba3b9e758ceb695e7621a63d2ae09e9b3093dc
Jeroen Roovers <jer@airfi.aero>
2018-01-15 16:32:07 +0100

    cfg80211: fully initialize old channel for event
    Paul reported that he got a report about undefined behaviour
    that seems to me to originate in using uninitialized memory
    when the channel structure here is used in the event code in
    nl80211 later.
    
    He never reported whether this fixed it, and I wasn't able
    to trigger this so far, but we should do the right thing and
    fully initialize the on-stack structure anyway.
    
    Reported-by: Paul Menzel <pmenzel+linux-wireless@molgen.mpg.de>
    Signed-off-by: Johannes Berg <johannes.berg@intel.com>

--- net/wireless/reg.c
+++ net/wireless/reg.c
@@ -988,8 +988,7 @@ static void handle_reg_beacon(struct wiphy *wiphy,
 	if (wiphy->flags & WIPHY_FLAG_DISABLE_BEACON_HINTS)
 		return;
 
-	chan_before.center_freq = chan->center_freq;
-	chan_before.flags = chan->flags;
+	chan_before = *chan;
 
 	if (chan->flags & IEEE80211_CHAN_PASSIVE_SCAN) {
 		chan->flags &= ~IEEE80211_CHAN_PASSIVE_SCAN;
