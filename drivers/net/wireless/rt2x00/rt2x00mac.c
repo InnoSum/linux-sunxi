5a1b81bfc50a8d6807f430f33bc664244a132887
Jeroen Roovers <jer@airfi.aero>
2018-05-01 08:38:40 +0200

    rt2x00: check against flushing empty queue
    We have check if queue is not empty when start flushing queues on
    by mac80211 callback, but we also can start flushing queues by internal
    driver calls. So move check into rt2x00queue_flush_queue() to assure
    we do not flush empty queue anytime.
    
    Additionally add warning if we start to kick empty queue as in such
    situation we set wrong index in the HW queue, what can confuse the HW
    and have various negative consequences.
    
    Signed-off-by: Stanislaw Gruszka <sgruszka@redhat.com>

--- drivers/net/wireless/rt2x00/rt2x00mac.c
+++ drivers/net/wireless/rt2x00/rt2x00mac.c
@@ -744,8 +744,7 @@ void rt2x00mac_flush(struct ieee80211_hw *hw, bool drop)
 		return;
 
 	tx_queue_for_each(rt2x00dev, queue)
-		if (!rt2x00queue_empty(queue))
-			rt2x00queue_flush_queue(queue, drop);
+		rt2x00queue_flush_queue(queue, drop);
 }
 EXPORT_SYMBOL_GPL(rt2x00mac_flush);
 
